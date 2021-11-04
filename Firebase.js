0dc3240885e18553a2f2abc85847f577a0f68c2d
Maurolixx <instagramlinkinstagram@gmail.com>
2021-11-04 18:27:18 +0100

    fix

--- Firebase.js
+++ Firebase.js
@@ -42,7 +42,7 @@ else {
 }}, 20);
 
 function apriLink(){
-  window.open("https://www.w3schools.com");
+  window.open("https://www.instagram.com/p/CRvXXtkFIrB/?utm_source=ig_web_copy_link");
 const dbRef = firebase.database().ref();
   dbRef.child("/link/link/link").get().then((snapshot) => {
     log(snapshot.val());
